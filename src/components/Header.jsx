1364661bff52d55c539c8292f6b3e8ab07df5ad6
cskitty <melody.yue.yu@gmail.com>
2022-09-09 11:50:06 -0700

    added ide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -107,7 +107,7 @@ export function Header() {
             <div className="hidden md:block">
               <NavLink href="/login">Sign in</NavLink>
             </div>
-            <Button href="https://www.starcoder.us/index.html" color="blue">
+            <Button href="/index.html" color="blue">
               <span>
                 Start <span className="hidden lg:inline">coding</span>
               </span>
